d6dcd98d518af58fa69c425b7be642d34246ccf5
Krish Paul <bidhata@gmail.com>
2017-06-21 04:34:29 +0530

    Error in <code> fix

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -2,7 +2,7 @@
 do
 
   function run(msg, matches)
-     bot_sendMessage(get_receiver_api(msg), '<code>' .. msg.from.first_name .. ',আমাকে বেকার Ping Pong করছেন কেন? Ping এর পরে destination দিতে হয় জানেন না বুঝি ? \n \n आप इतना ज्यादा Ping Pong क्यों कर रहें हैं? Ping के बाद उसका Destination देना नहीं सीखे क्या?<code>', true, msg.id, 'html')
+     bot_sendMessage(get_receiver_api(msg), '<code>' .. msg.from.first_name .. ',আমাকে বেকার Ping Pong করছেন কেন? Ping এর পরে destination দিতে হয় জানেন না বুঝি ? \n \n आप इतना ज्यादा Ping Pong क्यों कर रहें हैं? Ping के बाद उसका Destination देना नहीं सीखे क्या?</code>', true, msg.id, 'html')
   end
 
   return {
